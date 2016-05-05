555fb883ef00d56cb1d472eae5e198eb8b0d5c48
Suhail <suhailj17@gmail.com>
2016-05-05 14:13:38 -0400

    button does something

---
+++ app/src/main/java/com/mobile/stockr/stockr_android/GetQuote.java
@@ -0,0 +1,15 @@
+package com.mobile.stockr.stockr_android;
+
+/**
+ * Created by suhail on 5/5/2016.
+ */
+public class GetQuote {
+
+
+
+    public String getValue() {
+
+
+        return "hi";
+    }
+}
--- app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
+++ app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.mobile.stockr.stockr_android;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btnSearch = (Button) v;
+        TextView lblValueDisplay = (TextView) v;
+        lblValueDisplay.setText("TEST");
     }
 }
