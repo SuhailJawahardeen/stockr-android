df9352b35d43b8281cbc696e30e242746ee61f2b
Suhail <suhailj17@gmail.com>
2016-05-10 12:59:50 -0400

    not sure what i did here

--- app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
+++ app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
