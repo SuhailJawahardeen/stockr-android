794391fb13e708cf29c24f9109877e35afb409fb
Suhail <suhailj17@gmail.com>
2016-05-13 17:18:37 -0400

    progress!

--- app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
+++ app/src/main/java/com/mobile/stockr/stockr_android/HomeActivity.java
@@ -1,16 +1,22 @@
 package com.mobile.stockr.stockr_android;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,13 +25,38 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+
+
     }
+    Thread thread = new Thread(new Runnable()
+    {
+
+        @Override
+        public void run() {
+            try
+            {
+                EditText txtUserInput = (EditText) findViewById(R.id.txtUserInput);
+                TextView lblValueDisplay = (TextView) findViewById(R.id.lblValueDisplay);
+                String stkvalue = YahooFinance.get(txtUserInput.getText().toString().toUpperCase()).toString();
+                lblValueDisplay.setText(stkvalue);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+            }
+
+        }
+    });
 
     @Override
     public void onClick(View v) {
         Button btnSearch = (Button) v;
-        TextView lblValueDisplay = (TextView) v;
-        TextView t = (TextView) findViewById(R.id.lblValueDisplay);
-        t.setText("THIS WORKS");
+
+
+
+
+
     }
 }
